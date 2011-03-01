5d52ee8feb96dd1c11f448d2a082786903e4ca8b
marc.celani@gmail.com <marc.celani@gmail.com@47f79cf4-d25a-0985-2caa-a5515313f545>
2011-03-01 10:32:27 +0000

    Added the event and ModelComponent hierarchy

---
+++ trunk/ZRTSModel/ModelComponent.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ZRTSModel
+{
+    [Serializable()]
+    abstract class ModelComponent
+    {
+        // Composite Pattern members
+        private ModelComponent container = null;
+        private List<ModelComponent> children = new List<ModelComponent>();
+
+        // Observer Pattern members
+        private List<ModelComponentObserver> observers = new List<ModelComponentObserver>();
+        
+        // Composite Pattern Interface
+        public ModelComponent GetContainer()
+        {
+            return container;
+        }
+
+        public void SetContainer(ModelComponent composite)
+        {
+            container = composite;
+        }
+
+        public List<ModelComponent> GetChildren()
+        {
+            return children;
+        }
+
+        public void AddChild(ModelComponent child)
+        {
+            children.Add(child);
+        }
+
+        public void RemoveChild(ModelComponent child)
+        {
+            children.Remove(child);
+        }
+        
+
+        // Observer Pattern Interfaces
+        public void RegisterObserver(ModelComponentObserver observer)
+        {
+            observers.Add(observer);
+        }
+
+        public void UnregisterObserver(ModelComponentObserver observer)
+        {
+            observers.Remove(observer);
+        }
+
+        public void NotifyAll()
+        {
+            foreach (ModelComponentObserver o in observers)
+            {
+                o.notify(this);
+            }
+            if (container != null)
+            {
+                container.NotifyAll();
+            }
+        }
+
+        // Visitor Pattern Interfaces
+        public abstract void Accept(ModelComponentVisitor visitor);
+
+    }
+}
