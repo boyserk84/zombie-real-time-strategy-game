f6f034595606d557a6890366df79add41e436ec6
marc.celani@gmail.com <marc.celani@gmail.com@47f79cf4-d25a-0985-2caa-a5515313f545>
2011-03-01 11:11:18 +0000

    Added EntityList.cs as an example of a ModelComponent.  Updated ModelComponentVisitor.cs to handle EntityLists.  Updated TriggerDecorator to implement ModelComponentVisitor and ModelComponentObserver.  It handles it by completing a no op on all ModelComponentVisitor methods.  Finally, updated PlayerOutOfUnitsCondition to override visiting EntityList in order to determine whether or not the player has run out of entities (and if so, the condition is met).

---
+++ trunk/ZRTSModel/EntityList.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ZRTSModel.Entities;
+
+namespace ZRTSModel
+{
+    class EntityList : ModelComponent
+    {
+        public override void Accept(ModelComponentVisitor visitor)
+        {
+            visitor.visit(this);
+        }
+
+        internal static IEnumerable<Entity> GetList()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- trunk/ZRTSModel/Trigger/PlayerOutOfUnitsCondition.cs
+++ trunk/ZRTSModel/Trigger/PlayerOutOfUnitsCondition.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using ZRTSModel.Entities;
 
 namespace ZRTSModel.Trigger
 {
@@ -14,9 +15,24 @@ namespace ZRTSModel.Trigger
         {
             this.player = player;
         }
+
         public override bool CheckMyCondition(Scenario.Scenario scenario)
         {
             throw new NotImplementedException();
+        }
+
+        override public void Visit(EntityList entitylist)
+        {
+            isMet = true;
+            foreach (Entity e in EntityList.GetList())
+            {
+                if (e.getOwner() == player)
+                {
+                    isMet = false;
+                    break;
+                }
+            }
+            needsToBeEvaled = false;
         }
     }
 }
