c8e868b9dcc6d85c8d3bffac0388abddcc61c71a
marc.celani@gmail.com <marc.celani@gmail.com@47f79cf4-d25a-0985-2caa-a5515313f545>
2011-03-01 10:37:09 +0000

    Moved Event into Logic

---
+++ ZRTSLogic/Event/EventDelegator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ZRTSLogic.Event
+{
+    abstract class EventDelegator : NoOpEventHandler
+    {
+        private List<EventHandler> handlers = new List<EventHandler>();
+
+        public void RegisterEventHandler(EventHandler handler)
+        {
+            handlers.Add(handler);
+        }
+
+        public void UnregisterEventHandler(EventHandler handler)
+        {
+            handlers.Remove(handler);
+        }
+
+        protected void NotifyAll(Event e)
+        {
+            foreach (EventHandler handler in handlers)
+            {
+                e.Accept(handler);
+            }
+        }
+    }
+}
---
+++ ZRTSLogic/Event/EventQueue.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ZRTSLogic.Event
+{
+    class EventQueue
+    {
+        
+        private Queue<Event> queue = new Queue<Event>();
+        private List<EventDelegator> delegators = new List<EventDelegator>();
+
+        // Queueing Interface
+        public void Enqueue(Event e)
+        {
+            queue.Enqueue(e);
+        }
+
+        public Event Dequeue()
+        {
+            return queue.Dequeue();
+        }
+
+        public Event Peek()
+        {
+            return queue.Peek();
+        }
+
+        // Observable Interface
+        public void Register(EventDelegator delegator)
+        {
+            delegators.Add(delegator);
+        }
+
+        public void Unregister(EventDelegator delegator)
+        {
+            delegators.Remove(delegator);
+        }
+
+        // Process Events Interface
+        public void Process()
+        {
+            if (queue.Count != 0)
+            {
+                Event e = Dequeue();
+                foreach (EventDelegator delegator in delegators)
+                {
+                    e.Accept(delegator);
+                }
+            }
+        }
+
+    }
+}
