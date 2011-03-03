c27494490947058c35d0dd454bbc97971c5b1882
marc.celani@gmail.com <marc.celani@gmail.com@47f79cf4-d25a-0985-2caa-a5515313f545>
2011-03-03 06:50:49 +0000

    Added MapEditorCommand interface and PlayerDataGridAdapterCommitter to do the work of the PlayersForm.  This version now allows a user to add a PlayerComponent to the model, and to read the PlayerList.
    The following bugs are introduced with this commit:
    *Reading the players in the Players form does not display the correct starting gold, wood, or metal.  It also does not submit them when updating the model.
    *Cannot remove a player.

---
+++ ZRTSMapEditor/MapEditorCommand.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ZRTSMapEditor
+{
+    public interface MapEditorCommand
+    {
+        void Do();
+        void Undo();
+        bool CanBeDone();
+    }
+}
---
+++ ZRTSMapEditor/PlayerDataGridAdapterCommitter.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ZRTSModel;
+
+namespace ZRTSMapEditor
+{
+    class PlayerDataGridAdapterCommitter : MapEditorCommand
+    {
+        private List<PlayerDataGridAdapter> adapters;
+
+        private PlayerDataGridAdapterCommitter()
+        {
+        }
+
+        public PlayerDataGridAdapterCommitter(List<PlayerDataGridAdapter> adapters)
+        {
+            this.adapters = adapters;
+        }
+
+        public void Do()
+        {
+            foreach (PlayerDataGridAdapter adapter in adapters)
+            {
+                adapter.Do();
+            }
+        }
+
+        public void Undo()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool CanBeDone()
+        {
+            bool canBeDone = true;
+            Dictionary<string, Boolean> names = new Dictionary<string, Boolean>();
+            foreach (PlayerDataGridAdapter adapter in adapters)
+            {
+                canBeDone = canBeDone && adapter.CanBeDone();
+                if (!canBeDone)
+                {
+                    break;
+                }
+                names[adapter.Player_Name] = true;
+            }
+            if (canBeDone)
+            {
+                canBeDone = (names.Count == adapters.Count);
+            }
+            return canBeDone;
+        }
+    }
+}
