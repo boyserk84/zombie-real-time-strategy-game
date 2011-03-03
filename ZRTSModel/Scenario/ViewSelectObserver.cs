6870f17743cf4a69f96fc9ddbe62585470e63c16
ronnocotap@gmail.com <ronnocotap@gmail.com@47f79cf4-d25a-0985-2caa-a5515313f545>
2011-03-03 01:28:17 +0000

    Changes made to ViewSelectObserver.cs

---
+++ ZRTSModel/Scenario/ViewSelectObserver.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ZRTSModel.Entities;
+
+namespace ZRTSModel.Scenario
+{
+    /// <summary>
+    /// ViewSelectObserver Objects
+    /// Observes only the Unit(s) that are currently selected by the User
+    /// 
+    /// </summary>
+    //[Serializable()]
+    interface ViewSelectObserver
+    {
+        /// <summary>
+        /// Return List of the selected Units
+        /// 
+        /// </summary>
+        List<Unit> getSelectedUnits
+        { get; set; }
+
+    }
+}
