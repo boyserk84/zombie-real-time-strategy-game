11b6d12d73059b1eddf1bd69e24ac1b31002ca7c
marc.celani@gmail.com <marc.celani@gmail.com@47f79cf4-d25a-0985-2caa-a5515313f545>
2011-03-04 00:33:43 +0000

    Added UI for creating a new scenario (setting size - see i4).
    Fixed a bug in BetterScenarioView in which when the scenario changes, the view was not invalidated.

---
+++ branches/marc.celani/ZRTSMapEditor/UI/CreateNewScenarioDialog.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace ZRTSMapEditor.UI
+{
+    partial class CreateNewScenarioDialog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.nameBox = new System.Windows.Forms.TextBox();
+            this.nameLabel = new System.Windows.Forms.Label();
+            this.widthBox = new System.Windows.Forms.TextBox();
+            this.widthLabel = new System.Windows.Forms.Label();
+            this.heightBox = new System.Windows.Forms.TextBox();
+            this.heightLabel = new System.Windows.Forms.Label();
+            this.createButton = new System.Windows.Forms.Button();
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // nameBox
+            // 
+            this.nameBox.Location = new System.Drawing.Point(90, 30);
+            this.nameBox.Name = "nameBox";
+            this.nameBox.Size = new System.Drawing.Size(100, 20);
+            this.nameBox.TabIndex = 0;
+            // 
+            // nameLabel
+            // 
+            this.nameLabel.AutoSize = true;
+            this.nameLabel.Location = new System.Drawing.Point(46, 30);
+            this.nameLabel.Name = "nameLabel";
+            this.nameLabel.Size = new System.Drawing.Size(38, 13);
+            this.nameLabel.TabIndex = 1;
+            this.nameLabel.Text = "Name:";
+            this.nameLabel.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // widthBox
+            // 
+            this.widthBox.Location = new System.Drawing.Point(90, 56);
+            this.widthBox.Name = "widthBox";
+            this.widthBox.Size = new System.Drawing.Size(100, 20);
+            this.widthBox.TabIndex = 2;
+            // 
+            // widthLabel
+            // 
+            this.widthLabel.AutoSize = true;
+            this.widthLabel.Location = new System.Drawing.Point(46, 56);
+            this.widthLabel.Name = "widthLabel";
+            this.widthLabel.Size = new System.Drawing.Size(38, 13);
+            this.widthLabel.TabIndex = 3;
+            this.widthLabel.Text = "Width:";
+            this.widthLabel.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // heightBox
+            // 
+            this.heightBox.Location = new System.Drawing.Point(90, 82);
+            this.heightBox.Name = "heightBox";
+            this.heightBox.Size = new System.Drawing.Size(100, 20);
+            this.heightBox.TabIndex = 4;
+            // 
+            // heightLabel
+            // 
+            this.heightLabel.AutoSize = true;
+            this.heightLabel.Location = new System.Drawing.Point(43, 82);
+            this.heightLabel.Name = "heightLabel";
+            this.heightLabel.Size = new System.Drawing.Size(41, 13);
+            this.heightLabel.TabIndex = 5;
+            this.heightLabel.Text = "Height:";
+            this.heightLabel.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // createButton
+            // 
+            this.createButton.Location = new System.Drawing.Point(23, 108);
+            this.createButton.Name = "createButton";
+            this.createButton.Size = new System.Drawing.Size(75, 23);
+            this.createButton.TabIndex = 6;
+            this.createButton.Text = "Create";
+            this.createButton.UseVisualStyleBackColor = true;
+            this.createButton.Click += new System.EventHandler(this.createButton_Click);
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.Location = new System.Drawing.Point(144, 107);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(75, 23);
+            this.cancelButton.TabIndex = 7;
+            this.cancelButton.Text = "Cancel";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            this.cancelButton.Click += new System.EventHandler(this.cancelButton_Click);
+            // 
+            // CreateNewScenarioDialog
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(250, 142);
+            this.Controls.Add(this.cancelButton);
+            this.Controls.Add(this.createButton);
+            this.Controls.Add(this.heightLabel);
+            this.Controls.Add(this.heightBox);
+            this.Controls.Add(this.widthLabel);
+            this.Controls.Add(this.widthBox);
+            this.Controls.Add(this.nameLabel);
+            this.Controls.Add(this.nameBox);
+            this.Name = "CreateNewScenarioDialog";
+            this.Text = "Create New Scenario";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox nameBox;
+        private System.Windows.Forms.Label nameLabel;
+        private System.Windows.Forms.TextBox widthBox;
+        private System.Windows.Forms.Label widthLabel;
+        private System.Windows.Forms.TextBox heightBox;
+        private System.Windows.Forms.Label heightLabel;
+        private System.Windows.Forms.Button createButton;
+        private System.Windows.Forms.Button cancelButton;
+    }
+}
