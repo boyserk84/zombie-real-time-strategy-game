0b2e0e3593595043199cd0181e15ea58c10888f4
dgg1988 <dgg1988@47f79cf4-d25a-0985-2caa-a5515313f545>
2011-03-04 18:49:22 +0000

    Fixed error when inserting a StaticEntity.

--- branches/dgg1988/ZRTSModel/GameWorld/Map.cs
+++ branches/dgg1988/ZRTSModel/GameWorld/Map.cs
@@ -69,21 +69,22 @@ namespace ZRTSModel.GameWorld
                 return false;
             if (y < 0 || y + e.height > this.height)
                 return false;
-            for (int j = 0; j < this.height; j++)
+            for (int j = y; j < y + e.height; j++)
             {
-                for (int i = 0; i < this.width; i++)
+                for (int i = x; i < x + e.width; i++)
                 {
-                    if (!cells[i, j].isValid)
+                    if (!cells[i,j].isValid)
                         return false;
                 }
             }
 
             // set the appropriate Cell StaticEntity pointers to the given StaticEntity
-            for (int j = 0; j < e.height; j++)
+            for (int j = y; j < y + e.height; j++)
             {
-                for (int i = 0; i < e.width; i++)
+                for (int i = x; i < x + e.width; i++)
                 {
                     cells[i, j].entity = e;
+					cells[i, j].isValid = false;
                 }
             }
 
