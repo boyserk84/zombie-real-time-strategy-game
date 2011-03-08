f7f70bcc0476414c96e1008e8e26929910598451
marc.celani@gmail.com <marc.celani@gmail.com@47f79cf4-d25a-0985-2caa-a5515313f545>
2011-03-08 07:33:54 +0000

    Added in files I forgot in last commit.

---
+++ branches/marc.celani/ZRTSMapEditor/ContainsChildVisitor.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ZRTSModel;
+using ZRTSMapEditor.MapEditorModel;
+
+namespace ZRTSMapEditor
+{
+    /// <summary>
+    /// A visitor that visits the a type given at construction that searches the children of the accepting component
+    /// for a given child.  It is only meant to evaluate a single accepting class.
+    /// </summary>
+    public class ContainsChildVisitor : NoOpMapEditorModelVisitor
+    {
+        private ModelComponent child = null;
+        private Type modelType = null;
+        private bool evaluated = false;
+        private bool containsChild = false;
+
+        private ContainsChildVisitor()
+        { }
+
+        public ContainsChildVisitor(ModelComponent targetChild, Type type)
+        {
+            child = targetChild;
+            modelType = type;
+        }
+
+        /// <summary>
+        /// Checks the accepting component to see if it is the type we are looking for, and if so, checks for the child given at construction.
+        /// Updates the state of the visitor and ensures that it will not be evaluated again.
+        /// </summary>
+        /// <param name="component"></param>
+        public override void Visit(ModelComponent component)
+        {
+            if (modelType.GetType().Equals(component.GetType()) && !evaluated)
+            {
+                foreach (ModelComponent mc in component.GetChildren())
+                {
+                    if (mc == child)
+                    {
+                        containsChild = true;
+                        break;
+                    }
+                }
+                evaluated = true;
+            }
+        }
+
+        public bool FoundChild()
+        {
+            return containsChild;
+        }
+        public bool WasEvaled()
+        {
+            return evaluated;
+        }
+    }
+}
