08f9ef9b10a57dccf600c26b88307c371cf7c58d
joegonzalez3 <joegonzalez3@47f79cf4-d25a-0985-2caa-a5515313f545>
2011-03-16 01:12:59 +0000

    added support for changing multiple tile types with a click and drag operation.
    will leave the cursor to be changed later.

--- ZRTSMapEditor/UI/TileUI.cs
+++ ZRTSMapEditor/UI/TileUI.cs
@@ -6,6 +6,7 @@ using System.Windows.Forms;
 using ZRTSModel;
 using ZRTSModel.GameWorld;
 using ZRTSModel.EventHandlers;
+using System.Diagnostics;
 
 namespace ZRTSMapEditor
 {
@@ -36,12 +37,8 @@ namespace ZRTSMapEditor
                 TileFactory tf = TileFactory.Instance;
                 this.Image = tf.getBitmapImproved(observable.GetTile());
             }
+            ((Control)this).AllowDrop = true;
 
-        }
-        protected override void OnClick(EventArgs e)
-        {
-            controller.OnClickMapCell(cell);
-            base.OnClick(e);
         }
 
         private void ChangeTile(Object sender, TileChangedEventArgs args)
@@ -59,9 +56,36 @@ namespace ZRTSMapEditor
             // 
             this.Margin = new System.Windows.Forms.Padding(0);
             this.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.TileUI_DragEnter);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.TileUI_MouseDown);
             ((System.ComponentModel.ISupportInitialize)(this)).EndInit();
             this.ResumeLayout(false);
 
         }
+
+        private void TileUI_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                this.DoDragDrop("paint", DragDropEffects.None);
+                controller.OnClickMapCell(cell);
+                base.OnClick(e);
+            }
+        }
+
+        private void TileUI_DragEnter(object sender, DragEventArgs e)
+        {
+            if (!e.Data.GetDataPresent(typeof(String)))
+                return;
+
+            if (e.Data.GetData(typeof(String)).Equals("paint"))
+            {
+                e.Effect = DragDropEffects.None;
+                controller.OnClickMapCell(cell);
+                base.OnClick(e);
+            }
+        }
+
+
     }
 }
