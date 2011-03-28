e67dfc706750007d5eba19f0278fe7d3e05d7d44
nkemav2 <nkemav2@47f79cf4-d25a-0985-2caa-a5515313f545>
2011-03-28 22:29:46 +0000

    Refactoring View, ViewUtilities, ViewGame, ViewSelect, and ViewUnitMenu. They are all inherited from ViewAbstract.

---
+++ branches/viewRefactoring/ZRTS/ZRTS/View/ViewGame.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ZRTS.View
+{
+    /// <summary>
+    /// viewGame
+    /// Base:View
+    /// 
+    /// This class will handle graphical contents of all game components regardless of user's interaction with the unit.
+    /// </summary>
+    public class ViewGame:ViewAbstract
+    {
+    }
+}
