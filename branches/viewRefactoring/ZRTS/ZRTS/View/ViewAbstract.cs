a6fa2e98622b74e520dc208d49b0f53829c4a74e
ronnocotap@gmail.com <ronnocotap@gmail.com@47f79cf4-d25a-0985-2caa-a5515313f545>
2011-03-28 22:18:43 +0000

    add ViewAbstract.cs

---
+++ branches/viewRefactoring/ZRTS/ZRTS/View/ViewAbstract.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using ZRTSModel.Entities;
+
+namespace ZRTS.View
+{
+    public abstract class ViewAbstract
+    {
+        public int width, height;
+
+
+        public virtual void loadSheet()
+        {
+
+        }
+
+        public virtual void draw()
+        {
+
+        }
+
+        public virtual float translateXScreen(float x)
+        {
+            return x;
+        }
+
+        public virtual float translateYScreen(float y)
+        {
+            return y;
+        }
+
+        public virtual void update()
+        {
+
+        }
+    }
+}
