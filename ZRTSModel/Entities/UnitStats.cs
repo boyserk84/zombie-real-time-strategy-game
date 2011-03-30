952e7b2336b298e1ebaba3862dc7f0a1b369bf4f
dgg1988 <dgg1988@47f79cf4-d25a-0985-2caa-a5515313f545>
2011-03-30 14:53:38 +0000

    Added comments to AttackAction and GuardAttack.
    AttackAction.work and GuardAttack.work both automatically return "true" if the unit given the commands cannot attack. (unit.stats.canAttack == false)
    
    Set UnitStats.canAttack to default to "true." 
    
    Added XML comments to UnitStats.cs.

--- ZRTSModel/Entities/UnitStats.cs
+++ ZRTSModel/Entities/UnitStats.cs
@@ -7,26 +7,76 @@ namespace ZRTSModel.Entities
 {
     public class UnitStats
     {
-        public string type = "";            // The type of the Unit.
+		/// <summary>
+		/// The type of the unit.
+		/// </summary>
+        public string type = "";
         /** UNIT STATS **/
-        public short maxHealth = 100;       // The maximum health of a unit.
-        public float speed = 0.1f;          // How much a Unit should move during a move cycle.
-        public float attackRange = 5.0f;    // How far a Unit can attack
-        public short attack = 10;           // How much damage a Unit does when it attacks.
-        public byte attackTicks = 10;       // How many ticks occur per attack cycle.
-        public float visibilityRange = 6.0f;// How far can the unit see.
-        public byte buildSpeed = 30;        // How much health a building gets per build cycle the Unit completes when building or repairing a building.
+		
+		/// <summary>
+		/// The maximum health of a unit.
+		/// </summary>
+        public short maxHealth = 100;
+
+		/// <summary>
+		/// How much a Unit should move during a move Cycle.
+		/// </summary>
+        public float speed = 0.1f;
+		/// <summary>
+		/// How far a Unit can Attack.
+		/// </summary>
+        public float attackRange = 5.0f;
+		/// <summary>
+		/// How much damage a Unit does when it attacks.
+		/// </summary>
+        public short attack = 10;
+		/// <summary>
+		/// How many ticks per attack cycle.
+		/// </summary>
+        public byte attackTicks = 10;
+		/// <summary>
+		/// How far the Unit can see.
+		/// </summary>
+        public float visibilityRange = 6.0f;
+		/// <summary>
+		/// How much health a building gets per cycle when a Unit is building or repairing a building.
+		/// </summary>
+        public byte buildSpeed = 30;
 
 		/** PRODUCTION COSTS **/
+		
+		///<summary>
+		/// Water cost to produce this Unit.
+		/// </summary>
         public byte waterCost = 0;
+		/// <summary>
+		/// Food cost to produce this unit.
+		/// </summary>
         public byte foodCost = 0;
+		/// <summary>
+		/// Lumber cost to produce this unit.
+		/// </summary>
         public byte lumberCost = 0;
+		/// <summary>
+		/// Metal cost to produce this unit.
+		/// </summary>
         public byte metalCost = 0;
 
-        /** UNIT ABILITIES **/
-        public bool canAttack = false;      // Can this Unit attack?
-        public bool canHarvest = false;     // Can this Unit harvest resources?
-        public bool canBuild = true;       // Can this Unit build buildings?
-        public bool isZombie = false;       // Is this Unit a zombie?
+		/// <summary>
+		/// Can this unit attack?
+		/// </summary>
+        public bool canAttack = true;
+		/// <summary>
+		/// Can this unit harvest resources?
+		/// </summary>
+        public bool canHarvest = false;
+		/// <summary>
+		/// Can this unit build buildings?
+		/// </summary>
+        public bool canBuild = true;
+		/// <summary>
+		/// Is this unit a Zombie?
+		/// </summary>
+        public bool isZombie = false;
     }
 }
