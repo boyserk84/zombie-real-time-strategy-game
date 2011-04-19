7e4f713c83f7ec5c47aae93b1caac47d76dfcc70
nkemav2 <nkemav2@47f79cf4-d25a-0985-2caa-a5515313f545>
2011-04-19 23:26:05 +0000

    Update VictoryChange part and add graphic

--- ZRTS/ZRTS/XnaCompositeView/XnaUIFrame.cs
+++ ZRTS/ZRTS/XnaCompositeView/XnaUIFrame.cs
@@ -11,14 +11,27 @@ namespace ZRTS.XnaCompositeView
     {
         private SpriteBatch spriteBatch;
 
+        
+
         public XnaUIFrame(Game game) : base(game)
         {
             spriteBatch = new SpriteBatch(game.GraphicsDevice);
+            ((XnaUITestGame)game).Model.VictoryStateChangedHandler += onVictory;
         }
+
+        public void onVictory(object sender, ZRTSModel.EventHandlers.GameVictoryStateChangeEventArgs e)
+        {
+            // Show image
+            PictureBox image = new PictureBox(Game, new Rectangle(865, 1320,470, 92));
+            AddChild(image);
+            image.DrawBox = new Rectangle((1280/2) - 300, 720/2, 470, 92); // location on the screen
+        }
+
 
         protected override void onDraw(XnaDrawArgs e)
         {
             // Do nothing
+            
         }
 
         public override SpriteBatch GetSpriteBatch(XnaUIComponent requester)
