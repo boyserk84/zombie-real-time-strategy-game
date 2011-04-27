b8aa038d01a03410280a5f805261a2f1efa35320
dgg1988 <dgg1988@47f79cf4-d25a-0985-2caa-a5515313f545>
2011-04-27 22:26:08 +0000

    Finished implementing WinWhenAllEnemyUnitsDead trigger. Added PlayerWinAction.cs.

---
+++ ZRTSModel/GameModel/Scenario/Trigger/PlayerWinAction.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ZRTSModel;
+
+namespace ZRTSModel.Trigger
+{
+	public class PlayerWinAction : Action
+	{
+		ScenarioComponent scenario;
+		
+		public PlayerWinAction(Trigger decorated, ScenarioComponent scenario) : base(decorated)
+		{
+			this.scenario = scenario;
+		}
+
+		public override void  PerformActions()
+		{
+			((GameModel.GameModel)scenario.Parent).VictoryState = GameModel.GameModel.GameVictoryState.PlayerWin;
+		}
+
+		public override void PerformMyAction()
+		{
+			PerformActions();
+		}
+	}
+}
